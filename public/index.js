5e7e220656f94f9ba32fa490ef7caa67cfb68a84
Gabi Procell <gprocell927@gmail.com>
2017-04-27 17:51:00 -0600

    Renders minimal required info to page. Working on saving a location. Time limit has been met

--- public/index.js
+++ public/index.js
@@ -1,6 +1,8 @@
 const city = document.querySelector('.city-input')
 const state = document.querySelector('.state-input')
 const zipcode = document.querySelector('.zipcode-input')
+const dataDisplay = document.querySelector('.solar-data')
+const totalInstalls = document.querySelector('.total-installs')
 let gabiKey
 
 document.querySelector('.zipcode-input').addEventListener('change', ()=> {
@@ -23,7 +25,7 @@ document.querySelector('.state-input').addEventListener('change', () => {
 
 document.querySelector('.location-form').addEventListener('submit', (e) => {
   e.preventDefault()
-  const server = (`http://developer.nrel.gov/api/solar/open_pv/installs/index?api_key=${gabiKey}&state=${state.value}`)
+  const server = (`http://developer.nrel.gov/api/solar/open_pv/installs/summaries?api_key=${gabiKey}&state=${state.value}`)
   fetch(server, {
     method: 'GET',
     headers: {
@@ -32,9 +34,32 @@ document.querySelector('.location-form').addEventListener('submit', (e) => {
     },
   })
     .then(res => res.json())
-    .then(res => console.log(res))
+    .then(res => dataDisplay.innerHTML = `<ul>
+                <li>City: ${city.value || 'undefined'}</li>
+                <li>State: ${state.value || 'undefined'}</li>
+                <li>Zipcode: ${zipcode.value || 'undefined'}</li>
+                <li>Total Installs: ${res.result.total_installs}</li>
+                <li>Cap: ${res.result.total_capacity} Mw</li>
+                <li>Total Cost: $${res.result.total_installs_with_cost}</li>
+                <li>Date that data was taken: ${Date()}</li>
+              </ul> <button class="save-button">Save This Location</button>`)
 })
 
+// document.querySelector('.save-button').addEventListener('click',(e) => {
+//   const server = ('/api/locations')
+//   fetch(server, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Accept': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       id: Date.now(),
+         content: dataDisplay.innerHTML.value(),
+//     })
+//   })
+//
+// })
 function getMyKey(){
   const server = ('http://localhost:3000/key')
   fetch(server, {
